0772c0e5417067ccb687a148161a95d7fbdc4ab8
AzureNights <kokilavanielamparidhi@gmail.com>
2025-05-24 18:05:49 +0800

    Got my api key configured and working, checked as well

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,30 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
+from google import genai
 
 
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+client = genai.Client(api_key=GEMINI_API_KEY)
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain some kanji"
+)
+# print(response.text)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def test_apikey():
+    if GEMINI_API_KEY:
+        api_key_status = "Loaded Successfully!"
+    else:
+        api_key_status = "Not found. Oops."
+
+
+    return f"Yay! App is running. Gemini Key Status: {api_key_status}"
 
 @app.route("/questions")
 def questions():

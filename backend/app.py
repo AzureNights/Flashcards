6c8c67f83e276b2c4d9dfb7116299cb3c5a2f64e
AzureNights <kokilavanielamparidhi@gmail.com>
2025-06-09 16:57:53 +0800

    edited client name, cleaned up the code, added a Post Endpoint and checked that it worked.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 from dotenv import load_dotenv
 import os
 from google import genai
@@ -8,8 +8,8 @@ load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-client = genai.Client(api_key=GEMINI_API_KEY)
-response = client.models.generate_content(
+gemini_client = genai.Client(api_key=GEMINI_API_KEY)
+response = gemini_client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain some kanji"
 )
 # print(response.text)
@@ -24,18 +24,25 @@ def test_apikey():
         api_key_status = "Not found. Oops."
 
 
-    if client:
+    if gemini_client:
         client_status = "Yay. Client was initialized too!"
     else:
         client_status = "Not initialized :("
 
 
-    return f"Yay! App is running. API Key Status: {api_key_status} AI Status: {client_status}"
+    return f"Yay! App is running. <br> API Key Status: {api_key_status} <br> AI Status: {client_status}"
 
-@app.route("/questions")
-def questions():
-    return {"questions":
-            ["nani desu ka? = whats that", "yes"]}
+# Started off with a json endpoint 
+# @app.route("/questions")
+# def questions():
+#     return {"questions":
+#             ["nani desu ka? = whats that", "yes"]}
+
+@app.route("/api/simple-prompt", methods = ["POST"])
+def simple_prompt():
+    return jsonify({
+        "message": "This endpointworks YAY!"
+    })
 
 
 # main driver func
